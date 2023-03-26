3d5ef9528510296c4994ccaff611bd36363d16cf
mabdullah <mabdullah2037@gmail.com>
2023-03-27 00:45:20 +0500

    array and object

--- app.js
+++ app.js
@@ -2,12 +2,41 @@
 // const inputs = document.querySelectorAll("input");
 // console.log(inputs);
 // console.log("usman");
+//BASIC TYPES
 // let names = "Abdullah";
 // let age = 10;
 // let isAvai = true;
 // // names = 123; xxx typescript give an error because the names type is string.
 // names = "usman"
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(52));
+// const circ = (diameter : number)=>{
+//     return diameter * Math.PI;
+// }
+// console.log(circ(52));
+// ARRAY 
+// var names = ['abdullah', 'usman', 'uzair'];
+// names.push('ammar');
+// // names.push(3);
+// // names[1] = 3;
+// var numbers = [10, 20, 12, 15];
+// numbers.push(25);
+// // numbers.push('ammar');
+// // numbers[0] = 'usman';
+// var mixed = ['uzair', 4, 'abdullah', 8, 9];
+// mixed.push('usman');
+// mixed.push(10);
+// mixed[0] = 3;
+// OBJECTS
+// var ninja = {
+//     name: 'Muhammad',
+//     belt: 'black',
+//     age: 30
+// };
+// ninja.age = 40;
+// ninja.name = 'abdullah';
+// // ninja.age = '30';
+// // ninja.skills = ['fighting', 'sneaking']
+// ninja = {
+//     name: 'Usman',
+//     belt: 'orange',
+//     age: 40
+// };
--- app.ts
+++ app.ts
@@ -5,6 +5,25 @@
 
 // console.log("usman");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BASIC TYPES
+
+
+
 // let names = "Abdullah";
 // let age = 10;
 // let isAvai = true;
@@ -13,8 +32,70 @@
 // names = "usman"
 
 
-const circ = (diameter : number)=>{
-    return diameter * Math.PI;
-}
+// const circ = (diameter : number)=>{
+//     return diameter * Math.PI;
+// }
 
-console.log(circ(52));
+// console.log(circ(52));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ARRAY 
+
+
+
+// var names = ['abdullah', 'usman', 'uzair'];
+// names.push('ammar');
+// // names.push(3);
+// // names[1] = 3;
+// var numbers = [10, 20, 12, 15];
+// numbers.push(25);
+// // numbers.push('ammar');
+// // numbers[0] = 'usman';
+// var mixed = ['uzair', 4, 'abdullah', 8, 9];
+// mixed.push('usman');
+// mixed.push(10);
+// mixed[0] = 3;
+
+
+
+
+
+
+
+
+
+
+// OBJECTS
+// var ninja = {
+//     name: 'Muhammad',
+//     belt: 'black',
+//     age: 30
+// };
+// ninja.age = 40;
+// ninja.name = 'abdullah';
+// // ninja.age = '30';
+// // ninja.skills = ['fighting', 'sneaking']
+// ninja = {
+//     name: 'Usman',
+//     belt: 'orange',
+//     age: 40
+// };
