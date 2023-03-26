f804836f14d1165e571aa802fa0a47bc77a07154
mabdullah <mabdullah2037@gmail.com>
2023-03-27 00:26:42 +0500

    basic type completed

--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
 // console.log("abdullah");
+
 // const inputs = document.querySelectorAll("input");
 // console.log(inputs);
+
 // console.log("usman");
+
 // let names = "Abdullah";
 // let age = 10;
 // let isAvai = true;
+
 // // names = 123; xxx typescript give an error because the names type is string.
 // names = "usman"
-var circ = function (diameter) {
+
+
+const circ = (diameter : number)=>{
     return diameter * Math.PI;
-};
+}
+
 console.log(circ(52));
